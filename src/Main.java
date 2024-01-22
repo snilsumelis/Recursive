d51cebd2f88f837617942138609c500e1d816ac4
nilsu melis sönmez <80063055+snilsumelis@users.noreply.github.com>
2024-01-22 17:09:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Taban değeri giriniz: ");
+        int taban = scanner.nextInt();
+
+        System.out.print("Üs değerini giriniz: ");
+        int us = scanner.nextInt();
+
+        int sonuc = usHesapla(taban, us);
+        System.out.println("Sonuç: " + sonuc);
+
+        scanner.close();
+    }
+
+    static int usHesapla(int taban, int us) {
+        if (us == 0) {
+            return 1;
+        } else {
+            return taban * usHesapla(taban, us - 1);
+        }
+    }
+}
